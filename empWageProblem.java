0e08cd23162e01b852b1d575beb1f9dc5a29e82f
Sanaqamar123 <sana.qamr@gmail.com>
2020-09-29 16:25:41 +0530

    Refactoring the code

--- empWageProblem.java
+++ empWageProblem.java
@@ -1,65 +1,35 @@
 public class empWageProblem {
-
-	public static void main(String[] args) {
-
-	System.out.println("Welcome to Employee Wage Calculation");
-
-	//check employee is present or not
-	double num1 = Math.floor(Math.random()*10)%2;
-
-		if(num1 == 0) {
-			System.out.println("Employee is present");
+	
+	public static final int PartTime=8;
+	public static final int FullTime=8;
+	public static final int WagePerHour=20;
+	public static final int WorkingDays=20;
+	public static final int MaxHoursInMonth=100;
+	//refactoring the code of employee wage computaton problem
+	public static int refactorEmpWageProblem() {
+		int WorkHours=0, totalHours=0, days=0;
+		while(totalHours <= MaxHoursInMonth && days < WorkingDays) {
+			days++;
+			int choice = (int)Math.floor(Math.random()*10)%3;
+			switch(choice) {
+			case 1 :
+				WorkHours=8;
+				break;
+			
+			case 2 :
+				WorkHours=8;
+				break;
+			default :
+				WorkHours=0;
+			}
+			totalHours+=WorkHours;
+			System.out.println("Day : "+days+"Hours :"+totalHours);
 		}
-		else
-			System.out.println("Employee is absent");
-
-	//Calculate full time daily wage of employee
-	int WagePerHour=20;
-	int FullDayHour=8;
-	int WagePerDay=WagePerHour*FullDayHour;
-	System.out.println("Wage Per day : "+WagePerDay);
-
-	//Calculate part time wage of employee
-	int PartTimeHour=8;
-	int WagePartTimePerDay=WagePerHour*PartTimeHour;
-	System.out.println("Part time wage per day : "+WagePartTimePerDay);
-
-	//Getting work hour by using switch case
-	int choice = (int)(Math.floor(Math.random()*10)%3);
-	switch(choice) {
-	case 1:
-		System.out.println("Employee is full time");
-		int WorkHour=8;
-		System.out.println("Work hour : "+WorkHour);
-		break;
-
-	case 2:
-		System.out.println("Employee is part time");
-		WorkHour=8;
-		System.out.println("Work hour : "+WorkHour);
-		break;
-
-	default :
-		System.out.println("Employee absent");
-		break;
+		int totalWage=totalHours*WagePerHour;
+		System.out.println("Total employee Wage : "+totalWage);
+		return totalWage;
 	}
-
-	//Calculating wages for a month
-	int WorkingDayPerMonth=20;
-	int TotalWagePerMonth=WorkingDayPerMonth*WagePerDay;
-	System.out.println("Total wage per month : "+TotalWagePerMonth);
-
-	//Condition continue till the maximum days and maximum hours is reached for a month 
-	int day=1;
-	int HoursInDay=8;
-	int MaxWorkingHours=100;
-	while(day <= WorkingDayPerMonth && HoursInDay <= 100)
-	{
-		int Wage=WagePerDay*day;
-		System.out.println("Wage for "+day+" : "+Wage);
-		day=day+1;
-		HoursInDay=HoursInDay+8;
-	}
-
+	public static void main(String[] args) {
+		refactorEmpWageProblem();
 	}
 }
