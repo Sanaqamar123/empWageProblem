d1f1f96c7eeb50694e4ffa054c480930fc4292a5
Sanaqamar123 <sana.qamr@gmail.com>
2020-09-29 14:26:44 +0530

    Check attendance of employee

--- empWageProblem.java
+++ empWageProblem.java
@@ -4,5 +4,14 @@ public class empWageProblem {
 
 	System.out.println("Welcome to Employee Wage Calculation");
 
+	//check employee is present or not
+	double num1 = Math.floor(Math.random()*10)%2;
+
+		if(num1 == 0) {
+			System.out.println("Employee is present");
+		}
+		else
+			System.out.println("Employee is absent");
+
 	}
 }
