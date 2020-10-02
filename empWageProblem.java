174c85747da2f475667b36bfcfe3278d3e064953
Sanaqamar123 <sana.qamr@gmail.com>
2020-10-02 22:53:28 +0530

    Calculating employee wage for multiple companies

--- empWageProblem.java
+++ empWageProblem.java
@@ -1,35 +1,72 @@
-public class empWageProblem {
-	
-	public static final int PartTime=8;
-	public static final int FullTime=8;
-	public static final int WagePerHour=20;
-	public static final int WorkingDays=20;
-	public static final int MaxHoursInMonth=100;
-	//refactoring the code of employee wage computaton problem
-	public static int refactorEmpWageProblem() {
+import java.util.*;
+
+class empWage {
+
+	public static final int PartTime=1;
+	public static final int FullTime=2;
+
+}
+
+class empWage1 extends empWage{
+
+	Scanner sc = new Scanner(System.in);
+
+	public int computeEmpWage(int wagePerHour,int workingDays,int maxHoursInMonth) {
+
 		int WorkHours=0, totalHours=0, days=0;
-		while(totalHours <= MaxHoursInMonth && days < WorkingDays) {
+
+		while(totalHours <= maxHoursInMonth && days < workingDays) {
+
 			days++;
 			int choice = (int)Math.floor(Math.random()*10)%3;
+
 			switch(choice) {
 			case 1 :
 				WorkHours=8;
 				break;
-			
+
 			case 2 :
 				WorkHours=8;
 				break;
 			default :
 				WorkHours=0;
 			}
+
 			totalHours+=WorkHours;
-			System.out.println("Day : "+days+"Hours :"+totalHours);
 		}
-		int totalWage=totalHours*WagePerHour;
-		System.out.println("Total employee Wage : "+totalWage);
+
+		int totalWage=totalHours*wagePerHour;
+		System.out.println("Enter company name");
+		String name = sc.nextLine();
+		System.out.println("Company Name : "+name+"\nTotal employee Wage : "+totalWage+"\n");
 		return totalWage;
 	}
+}
+
+public class empWageProblem extends empWage1 {
+
 	public static void main(String[] args) {
-		refactorEmpWageProblem();
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("How Many Company's wages you want to calculate  : ");
+        int n = sc.nextInt();
+        empWageProblem Company[] = new empWageProblem[n];
+
+        for (int i = 0; i < n; i++) {
+
+        	System.out.println("Enter wage per day : ");
+        	int wage_per_day = sc.nextInt();
+
+        	System.out.println("Total working days : ");
+        	int working_days = sc.nextInt();
+
+        	System.out.println("Maximum hours in a month");
+        	int max_hours_in_month = sc.nextInt();
+	        Company[i] = new empWageProblem();
+        	Company[i].computeEmpWage(wage_per_day,working_days,max_hours_in_month);
+
+        }
+        sc.close();
 	}
 }
